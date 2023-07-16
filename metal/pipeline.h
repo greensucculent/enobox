9df6cd490538d47e956c9edf22f0046df6d2db10
Sam <copperboat@hey.com>
2023-07-16 17:57:46 -0400

    stash changes

---
+++ metal/pipeline.h
@@ -0,0 +1,23 @@
+// go:build darwin
+//  +build darwin
+
+#ifndef HEADER_PIPELINE
+#define HEADER_PIPELINE
+
+#import <Metal/Metal.h>
+
+// Structure of various metal resources needed to execute a computational
+// process on the GPU. We have to bundle this in a header that cgo doesn't
+// import because of a bug in LLVM that leads to a compilation error of "struct
+// size calculation error off=8 bytesize=0". Doesn't seem to be another solution
+// to this at the moment.
+typedef struct {
+  // Metal resources
+  id<MTLComputePipelineState> pipeline;
+  id<MTLCommandQueue> commandQueue;
+
+  // Buffers that will be supplied as arguments to the metal function
+  // NSMutableArray *buffers;
+} _pipeline;
+
+#endif
---
+++ metal/pipeline.m
@@ -0,0 +1,4 @@
+// go:build darwin
+//  +build darwin
+
+#include "pipeline.h"
