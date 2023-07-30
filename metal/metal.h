0a2d7cf76de53cb1ae36dce36bc4b790102aaf24
Sam <copperboat@hey.com>
2023-07-29 20:54:59 -0400

    metal: update public functions

--- metal/metal.h
+++ metal/metal.h
@@ -1,8 +1,21 @@
 // go:build darwin
 //  +build darwin
 
+#ifndef HEADER_METAL
+#define HEADER_METAL
+
 #include <stdlib.h>
 
-int initKernel(const char *metalCode, const char *funcName);
-void *addBuffer(int kernelId, int numBytes);
-void run(int kernelId);
+// Functions that must be called once for every application
+void metal_init();
+
+// Functions that must be called once for every metal function
+int metal_newFunction(const char *metalCode, const char *funcName);
+void metal_runFunction(int pipelineId, int *bufferIds, int numBufferIds);
+
+// Functions that must be called once for every buffer used as an argument to a
+// metal function
+int metal_newBuffer(int numElems, int elemSize);
+void *metal_retrieveBuffer(int bufferId);
+
+#endif
